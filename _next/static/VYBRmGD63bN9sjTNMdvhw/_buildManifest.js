f26751b11910111f3cc6f5f1f7b0ee456ca7e01c
Deepak Meena <deepakdude582@gmail.com>
2023-05-15 12:18:00 +0530

    .

---
+++ _next/static/VYBRmGD63bN9sjTNMdvhw/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/fafd2a86f303895c.css","static/chunks/pages/index-72d9ffc01214351b.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
